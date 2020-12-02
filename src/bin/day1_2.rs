018f9522154adfa2113dce4a9d4d3274427d03ee
Anders Sandström <anders.sandstrom89@gmail.com>
2020-12-02 22:23:50 +0100

    Optimization for day 1_2

--- src/bin/day1_2.rs
+++ src/bin/day1_2.rs
@@ -1,33 +1,37 @@
-
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::{self, BufReader};
 use std::collections::HashSet;
 
 fn main() -> io::Result<()> {
+    
     let input = File::open("input_day1_1")?;
     let input = BufReader::new(input);
 
-    let mut map: HashSet<i64> = HashSet::new();
+    let mut all: HashSet<i64> = HashSet::new();
+    let mut small: HashSet<i64> = HashSet::new();
 
     for line in input.lines() {
         let entry = line.unwrap();
         let entry = entry.parse().unwrap();
 
-        map.insert(entry);
-    }
+        all.insert(entry);
 
-    for entry in &map {
-        for entry2 in &map {
+        if entry < 1010{
+            small.insert(entry);
+        }
+    }
+    
+    for entry in &small {
+        for entry2 in &small {
             let entry3 = 2020 - entry - entry2;
 
-            if map.contains(&entry3) {
+            if all.contains(&entry3) {
                 println!("{}", entry * entry2 * entry3 );
                 return Ok(())
             }
         }
     }
 
-    println!("{}", "day1_1");
     Ok(())
 }
