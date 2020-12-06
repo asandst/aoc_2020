cb2f7a6f4a72270f100606e3cdedd37306decb61
Anders Sandström <anders.sandstrom89@gmail.com>
2020-12-06 18:12:31 +0100

    Made day 6_2 shorter

--- src/bin/day6_2.rs
+++ src/bin/day6_2.rs
@@ -1,8 +1,11 @@
+extern crate itertools;
 use std::collections::HashSet;
 use std::fs;
 use std::io;
+use itertools::Itertools;
 
 fn main() -> io::Result<()> {
-    println!("{}", fs::read_to_string("input_day6").unwrap().split("\r\n\r\n").map(|g| g.split("\r\n").fold(None, |acc, x| Some(acc.unwrap_or(x.chars().collect::<HashSet<char>>()).intersection(&x.chars().collect()).cloned().collect()))).map(|s| s.unwrap().len()).sum::<usize>());
+    let input = fs::read_to_string("input_day6").unwrap();
+    println!("{:?}", input.split("\r\n\r\n").map(|g| g.split("\r\n").map(|p| p.chars().collect::<HashSet<char>>()).fold1(|x, y| x.intersection(&y).cloned().collect())).map(|s| s.unwrap().len()).sum::<usize>());
     Ok(())
 }
