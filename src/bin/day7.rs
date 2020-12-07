97211ec9ca31ba166d9fca521642f0b759064079
Anders Sandström <anders.sandstrom89@gmail.com>
2020-12-07 18:24:48 +0100

    Minor refactoring of day 7

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -11,42 +11,41 @@ fn main() -> io::Result<()> {
     let input = File::open("input_day7")?;
     let input = BufReader::new(input);
 
-    let bag_regex = Regex::new(r"([0-9]+)(.+)").unwrap();
+    let bag_regex = Regex::new(r"([0-9]+)\s*(.+)\s*").unwrap();
     let mut bag_contents: HashMap<String, HashMap<String, usize>> = HashMap::new();
     let mut reverse_bag_contents: HashMap<String, HashSet<String>> = HashMap::new();
 
     for line in input.lines() {
-        let entry = line.unwrap();
-        let vec = entry.split("contain").collect::<Vec<&str>>();
-        let key = vec[0].trim();
-        let contents = vec[1].split(",").collect::<Vec<&str>>();
+        let line = line?;
+        let parts = line.split("contain").collect::<Vec<&str>>();
+        let key = parts[0].trim().to_string();
+        let contents = parts[1].split(",").collect::<Vec<&str>>();
 
         for content in contents{
             if content.trim() == "no other bags."{
-                bag_contents.insert(key.to_string(), HashMap::new());
+                bag_contents.insert(key.clone(), HashMap::new());
             } else {
                 let caps = bag_regex.captures(&content).unwrap();
-                let count: usize = caps.get(1).unwrap().as_str().parse().unwrap();
-                let name = caps.get(2).unwrap().as_str();
-                let name = name.replace(".", "");
-                let mut name = name.trim().to_string();
-    
-                if count == 1 {
-                    name = name + "s";
-                }
+                let count = caps.get(1).unwrap().as_str().parse().unwrap();
+                
+                let name = caps.get(2).unwrap().as_str().replace(".", "").to_string();
+                let name = match count {
+                    1 => name + "s",
+                    _ => name
+                };
 
-                let map = bag_contents.entry(key.to_string()).or_insert(HashMap::new());
-                map.insert(name.to_string(), count);
+                let map = bag_contents.entry(key.clone()).or_insert(HashMap::new());
+                map.insert(name.clone(), count);
 
-                let rev_map = reverse_bag_contents.entry(name.to_string()).or_insert(HashSet::new());
-                rev_map.insert(key.to_string());
+                let rev_map = reverse_bag_contents.entry(name).or_insert(HashSet::new());
+                rev_map.insert(key.clone());
             }
         }
     }
 
-    let mut set = HashSet::new();
-    can_contain_bag(&reverse_bag_contents, "shiny gold bags", &mut set);
-    println!("Part 1 {}", set.len()-1);
+    let mut bags = HashSet::new();
+    can_contain_bag(&reverse_bag_contents, "shiny gold bags", &mut bags);
+    println!("Part 1 {}", bags.len());
     println!("Part 2 {}", count_contents(&bag_contents, "shiny gold bags") - 1);
 
     Ok(())
@@ -56,9 +55,9 @@ fn can_contain_bag(reverse_bag_contents: &HashMap<String, HashSet<String>>, key:
     let def = HashSet::new();
     let possible = reverse_bag_contents.get(key).unwrap_or(&def);
     for bag in possible {
+        contains.insert(bag.to_string());
         can_contain_bag(reverse_bag_contents, bag, contains);
     }
-    contains.insert(key.to_string());
 }
 
 fn count_contents(map: &HashMap<String, HashMap<String, usize>>, key: &str) -> usize {
