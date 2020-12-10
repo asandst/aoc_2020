87816ac1026f3c42edcb8cc765613c0a1290652e
Anders Sandström <anders.sandstrom89@gmail.com>
2020-12-10 17:42:38 +0100

    refactor day 10

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -2,71 +2,53 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::{self, BufReader};
 use std::collections::BTreeMap;
-use std::collections::HashSet;
 
 fn main() -> io::Result<()> {
     let input = File::open("input_day10")?;
     let input = BufReader::new(input);
+
     let mut numbers: Vec<i64> = Vec::new();
-
-    for line in input.lines() {
-        let entry = line.unwrap();
-        let number = entry.parse::<i64>().unwrap();
-        numbers.push(number);
-    }
     numbers.push(0);
+    numbers.extend(input.lines().map(|line| line.unwrap().parse::<i64>().unwrap()));
     numbers.sort();
+    numbers.push(numbers.last().unwrap()+3);
 
-    let vals = numbers.iter();
-    let next_vals = numbers.iter().skip(1);
-
-    let diffs = vals
-        .zip(next_vals)
-        .map(|(cur, next)| next - cur)
-        .collect::<Vec<i64>>();
-
+    let diffs = numbers.iter().zip(numbers.iter().skip(1)).map(|(cur, next)| next - cur).collect::<Vec<i64>>();
     let num1 = diffs.iter().filter(|v| **v == 1).count();
-    let num3 = diffs.iter().filter(|v| **v == 3).count() + 1;
+    let num3 = diffs.iter().filter(|v| **v == 3).count();
     println!("Part1 {}", num1 * num3);
 
     let mut paths: BTreeMap<i64, Vec<i64>> = BTreeMap::new();
-    let numbers_hash = numbers.iter().map(|v| *v).collect::<HashSet<i64>>();
-
     for num in &numbers{
         for i in 1..4{
-            if numbers_hash.contains(&(num + i)) {
+            if numbers.contains(&(num + i)) {
                 paths.entry(*num).or_insert(Vec::new()).push(num + i);
             }
         }
     }
-    paths.insert(*numbers.last().unwrap(), Vec::new());
 
-    let mut counts: Vec<i64> = Vec::new();
     let mut start = 0;
     let mut last = 0;
+    let mut combinations = 1;
     for num in numbers {
-        if paths.get(&last).unwrap().len() <= 1 {
-            let mut count = 0;
-            count_paths_util(start, num, &mut count, &paths);
-            counts.push(count);
+        if paths.get(&last).unwrap().len() == 1 {
+            combinations *= count_paths(start, num, &paths);
             start = num;
         }
         last = num;
     }
 
-    let combs = counts.iter().map(|i| *i as i128).fold(1, |x, y| x * y);
-    println!("Part2 {}", combs);
-
+    println!("Part2 {}", combinations);
     Ok(())
 }
 
-fn count_paths_util(start: i64, stop: i64, path_count: &mut i64, paths: &BTreeMap<i64, Vec<i64>>) {
+fn count_paths(start: i64, stop: i64, paths: &BTreeMap<i64, Vec<i64>>) -> i64{
+    let mut path_count = 0;
     if start == stop {
-        *path_count += 1;
-    } else if start > stop{
-    }else {
-        for i in paths.get(&start).unwrap() {
-            count_paths_util(*i, stop, path_count, paths);
-        }
+        return path_count + 1;
+    } 
+    for i in paths.get(&start).unwrap() {
+        path_count += count_paths(*i, stop, paths);
     }
+    return path_count;
 }
