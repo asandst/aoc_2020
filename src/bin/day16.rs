925e1d53f786912065206735fa279d92b38e3b63
Anders Sandström <anders.sandstrom89@gmail.com>
2020-12-16 22:52:06 +0100

    Day 16 refactor to functional api

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -1,22 +1,17 @@
 extern crate regex;
 use regex::Regex;
-
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::{self, BufReader};
-use std::time::Instant;
 
 fn main() -> io::Result<()> {
-    
     let input = File::open("input_day16")?;
     let input = BufReader::new(input);
     let input = input.lines().map(|line| line.unwrap().to_string()).collect::<Vec<String>>();
 
     let field_valid_ranges_regex = Regex::new(r"([a-z ]+): ([0-9]+)\-([0-9]+) or ([0-9]+)\-([0-9]+)").unwrap();
     let csv_regex = Regex::new(r"([0-9]+,)+[0-9]+").unwrap();
-    let mut found_nearby_tickets = false;
-    let mut intervals : Vec<(usize, usize)> = Vec::new();
     let mut valid_tickets : Vec<Vec<usize>> = Vec::new();
     let mut ticket_fields : Vec<TicketField> = Vec::new();
     let mut my_ticket = Vec::new();
@@ -25,67 +20,45 @@ fn main() -> io::Result<()> {
     for line in &input {
         if field_valid_ranges_regex.is_match(&line){
             let caps = field_valid_ranges_regex.captures(&line).unwrap();
-            let name = caps.get(1).unwrap().as_str();
-            let number_lower_1 :usize= caps.get(2).unwrap().as_str().parse().unwrap();
-            let number_upper_1 :usize = caps.get(3).unwrap().as_str().parse().unwrap();
-
-            let number_lower_2 :usize = caps.get(4).unwrap().as_str().parse().unwrap();
-            let number_upper_2 :usize = caps.get(5).unwrap().as_str().parse().unwrap();
-
-            intervals.push((number_lower_1, number_upper_1));
-            intervals.push((number_lower_2, number_upper_2));
-
-            let mut i : Vec<(usize, usize)> = Vec::new();
-            i.push((number_lower_1, number_upper_1));
-            i.push((number_lower_2, number_upper_2));
-
-            ticket_fields.push(TicketField {intervals: i, name: name.to_string()})
+            let name = caps[1].to_string();
+            let intervals : Vec<(usize, usize)> = vec!{(caps[2].parse().unwrap(), caps[3].parse().unwrap()), (caps[4].parse().unwrap(), caps[5].parse().unwrap())};
+            ticket_fields.push(TicketField {intervals, name})
         } else if csv_regex.is_match(&line){
             let ticket = line.split(",").map(|n| n.parse().unwrap()).collect::<Vec<usize>>();
+            if my_ticket.len() > 0 {
+                let invalid_values = ticket.iter()
+                    .filter(|&value| !ticket_fields.iter().flat_map(|field| field.intervals.iter()).any(|(min, max)| value >= min && value <= max));
+                invalid_sum += invalid_values.clone().sum::<usize>();
 
-            if found_nearby_tickets {
-                let mut invalid_ticket = false;
-                for value in &ticket {
-                    let mut ok = false;
-                    for (min, max) in &intervals {
-                        if value >= min && value <= max {
-                            ok = true;
-                            break;
-                        }
-                    }
-
-                    if !ok {
-                        invalid_sum += value;
-                        invalid_ticket = true;
-                    }
-                }
-
-                if !invalid_ticket {
+                if invalid_values.count() == 0{
                     valid_tickets.push(ticket);
                 }
             } else {
                 my_ticket = ticket.clone();
-                valid_tickets.push(ticket);
             }
-        } else if line == "nearby tickets:"{
-            found_nearby_tickets = true;
         }
     }
     println!("Part 1 {}", invalid_sum);
-    let now = Instant::now();
+
     let mut solver = Solver {map: HashMap::new(), valid_tickets, ticket_fields};
-    let solution = solver.solve(Vec::new());
-
-    let mut prod = 1;
-    for (i, sol) in solution.unwrap().iter().enumerate(){
-        if sol.name.starts_with("departure"){
-            prod *= my_ticket[i];
-        }
-    }
+    let solution = solver.solve(&Vec::new());
+    let prod = solution.unwrap().iter().enumerate()
+        .filter(|(_, &sol)| solver.ticket_fields[sol].name.starts_with("departure"))
+        .map(|(i, _)| my_ticket[i])
+        .fold(1, |x, y| x*y);
     println!("Part 2 {:?}", prod);
-    println!("{:?}", now.elapsed().as_micros());
-
     Ok(())
+}
+
+enum Result{
+    Partial,
+    Full,
+    Fail
+}
+
+struct TicketField{
+    intervals : Vec<(usize, usize)>,
+    name : String
 }
 
 struct Solver{
@@ -95,48 +68,24 @@ struct Solver{
 }
 
 impl Solver{
-    fn solve(&mut self, solution: Vec<usize>) -> Option<Vec<TicketField>> {
-        let len = self.ticket_fields.len();
-        
-        let mut left : Vec<usize> = Vec::new();
-        for i in 0..len{
-            if !solution.contains(&i) {
-                left.push(i);
-            }
-        }
-    
-        left.sort_by_key(|&k| self.count(k));
-    
-        for l in left {
+    fn solve(&mut self, solution: &Vec<usize>) -> Option<Vec<usize>> {
+        let mut left : Vec<usize> = (0..self.ticket_fields.len()).into_iter().filter(|i| !solution.contains(i)).collect();
+        left.sort_by_cached_key(|&k| self.count(k));
+        left.iter().find_map(|&l| {
             let mut new_solution = solution.clone();
             new_solution.push(l);
-            let res = self.check(&new_solution);
-            if res == Result::Full {
-                let mut sol : Vec<TicketField> = Vec::new();
-                for i in new_solution{
-                    sol.push(self.ticket_fields[i].clone());
-                }
-    
-                return Some(sol);
-            } else if res == Result::Partial{
-                let sol = self.solve(new_solution);
-                if sol.is_some(){
-                    return sol;
-                }
+            match self.check(&new_solution){
+                Result::Full => Some(new_solution),
+                Result::Partial => self.solve(&new_solution),
+                Result::Fail => None
             }
-        }
-    
-        None
+        })
     }
 
     fn check(&mut self, solution: &Vec<usize>) -> Result {
-        for (j, &i) in solution.iter().enumerate(){
-            if !self.check_column(i, j){
-                return Result::Fail;
-            }
-        }
-        
-        if solution.len() == self.ticket_fields.len(){
+        if solution.iter().enumerate().any(|(j, &i)| !self.check_column(i, j)){
+            Result::Fail
+        } else if solution.len() == self.ticket_fields.len(){
             Result::Full
         } else {
             Result::Partial
@@ -147,30 +96,11 @@ impl Solver{
         let ticket_field = &self.ticket_fields[ticket_field_index];
         let key = self.ticket_fields.len()*ticket_field_index + i;
         let valid_tickets = &self.valid_tickets;
-        
+
         *self.map.entry(key).or_insert_with(|| valid_tickets.iter().all(|ticket| ticket_field.intervals.iter().any(|(min, max)| ticket[i] >= *min && ticket[i] <= *max)))
     }
 
     fn count(&mut self, ticket_field_index: usize) -> usize{
-        let mut count = 0;
-        for i in 0..self.valid_tickets[0].len(){
-            if self.check_column(ticket_field_index, i){
-                count += 1;
-            }
-        }
-        count
+        (0..self.valid_tickets[0].len()).into_iter().filter(|&i| self.check_column(ticket_field_index, i)).count()
     }
-}
-
-#[derive(Clone, Debug, PartialEq)]
-enum Result{
-    Partial,
-    Full,
-    Fail
-}
-
-#[derive(Clone, Debug)]
-struct TicketField{
-    intervals : Vec<(usize, usize)>,
-    name : String
 }
