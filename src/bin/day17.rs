1c2e622eff614644754ab5ef05cc0a0dd6db220f
Anders Sandström <anders.sandstrom89@gmail.com>
2020-12-17 23:59:30 +0100

    Making the code unreadable with functional api

--- src/bin/day17.rs
+++ src/bin/day17.rs
@@ -12,26 +12,11 @@ fn main() -> io::Result<()> {
     let input = input.lines().map(|line| line.unwrap().to_string()).collect::<Vec<String>>();
     let iter = -1..=1;
 
-    let mut map : HashSet<Vec<i64>> = HashSet::new();
-    for (y, l) in input.iter().enumerate(){
-        for (x, c) in l.chars().enumerate(){
-            if c == '#'{
-                map.insert(vec!{x as i64,y as i64,0});
-            }
-         }
-    }
-    
+    let map = input.iter().enumerate().map(|(y, l)| l.chars().enumerate().filter(|(_, c)| *c == '#').map(move |(x, _)| vec!{x as i64,y as i64,0})).flatten().collect::<HashSet<Vec<i64>>>();
     let count = run(map, &iproduct!(iter.clone(), iter.clone(), iter.clone()).map(|(x, y, z)| vec!{x, y, z}).filter(|vec| vec.iter().any(|&v| v!= 0)).collect());
     println!("Part 1 {}", count);
 
-    let mut map : HashSet<Vec<i64>> = HashSet::new();
-    for (y, l) in input.iter().enumerate(){
-        for (x, c) in l.chars().enumerate(){
-            if c == '#'{
-                map.insert(vec!{x as i64, y as i64, 0, 0});
-            }
-         }
-    }
+    let map = input.iter().enumerate().map(|(y, l)| l.chars().enumerate().filter(|(_, c)| *c == '#').map(move |(x, _)| vec!{x as i64,y as i64, 0, 0})).flatten().collect::<HashSet<Vec<i64>>>();
     let count = run(map, &iproduct!(iter.clone(), iter.clone(), iter.clone(), iter.clone()).map(|(x, y, z, w)| vec!{x, y, z, w}).filter(|vec| vec.iter().any(|&v| v!= 0)).collect());
     println!("Part 2 {}", count);
 
@@ -43,21 +28,19 @@ fn run(mut map: HashSet<Vec<i64>>, points: &Vec<Vec<i64>>) -> usize {
         let mut new_map : HashSet<Vec<i64>> = HashSet::new();
 
         for active in &map {
-            let mut active_neighbors = 0;
-            for v1 in points{
-                let key1 = izip!(active.iter(), v1.iter()).map(|(value, offset)| value + offset).collect::<Vec<i64>>();
-                if map.contains(&key1){
-                    active_neighbors += 1;
+            let points_gen = |v1 : Vec<i64>| points.iter().map(move |v2| izip!(v1.iter(), v2.iter()).map(|(value, offset)| value + offset).collect::<Vec<i64>>());
+            match points_gen(active.clone()).map(|key| {
+                if map.contains(&key){
+                    1
                 } else {
-                    let active_neighbors2 = points.iter().map(|v2| izip!(active.iter(), v1.iter(), v2.iter()).map(|(value, offset1, offset2)| value + offset1 + offset2).collect::<Vec<i64>>()).filter(|key| map.contains(key)).count();
-                    if active_neighbors2 == 3 {
-                        new_map.insert(key1.clone());
+                    if 3 == points_gen(key.clone()).filter(|key| map.contains(key)).count(){
+                        new_map.insert(key.clone());
                     }
+                    0
                 }
-            }
-
-            if active_neighbors >=2 && active_neighbors <=3{
-                new_map.insert(active.clone());
+            }).sum(){
+                2 | 3 => drop(new_map.insert(active.clone())),
+                _ => ()
             }
         }
 
